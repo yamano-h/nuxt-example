7eae642d655acecfe6f2e67bb35b2edf79751a8f
yamano-h <yamano_h@daitotools.com>
2019-04-13 16:35:40 +0900

    犬のリスト表示まで

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,24 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-example
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+    <section class="container">
+        <div class="columns is-multiline">
+
+            <!-- v-for で breed_list からループ出力 -->
+            <div v-for="(item, i) in breed_list" v-bind:key='i' class='column is-2'>
+                <a class="button">{{ i }}</a>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+    import dogApi from '@/api/dog';
+    import {mapState} from 'vuex';
 
-export default {
-  components: {
-    AppLogo
-  }
-}
+    export default {
+        async fetch({store}) {
+            let json = await dogApi.breeds();
+            store.commit('breed_list_update', json)
+        },
+        computed: mapState(['breed_list']),  // mapState ヘルパー
+    }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-

d009fb9d659a64bdd6ed7e53e49e139795641bf2
yamano-h <yamano_h@daitotools.com>
2019-04-13 16:57:35 +0900

    いいねできるとこまで

--- pages/dogs/_breed/index.vue
+++ pages/dogs/_breed/index.vue
@@ -1,10 +1,14 @@
 <template>
     <section class="container">
         <div class="columns is-multiline">
+            <div v-for="(item, i) in dog_list" v-bind:key="i" class="column is-2">
+                <img v-bind:src="item.url">
 
-            <!-- 追加 -->
-            <div v-for="(item, i) in dog_list" v-bind:key="i" class="column is-1">
-                <img :src="item.url">
+                <!-- 追加 -->
+                <span v-if="i < 3" class="tag is-danger">NEW</span>
+                <a class="button is-warning  is-small" v-on:click="item.like += 1">
+                    <span>いいね！{{item.like}}件</span>
+                </a>
             </div>
         </div>
     </section>
